3166c7a42ac9f59db928a92739b4056523625ca1
Carol-Rocha <caroldsr1@gmail.com>
2023-08-05 20:03:42 -0300

    feat: create client entity

---
+++ src/entities/clients.entity.ts
@@ -0,0 +1,47 @@
+import { getRounds, hashSync } from "bcryptjs"
+import {
+  Entity, 
+  PrimaryGeneratedColumn, 
+  Column, 
+  CreateDateColumn, 
+  UpdateDateColumn, 
+  DeleteDateColumn, 
+  BeforeInsert, 
+  BeforeUpdate} from "typeorm"
+
+@Entity("clients")
+export class Client {
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column({ type: "varchar", length: 120})
+    full_name: string
+
+    @Column({ type: "varchar", length: 50, unique: true })
+    user_name: string
+
+    @Column({ type: "varchar", length: 100})
+    password: string
+
+    @BeforeInsert()
+    @BeforeUpdate()
+    hashPassword() {
+      const isEncrypted: number = getRounds(this.password)
+
+      if (!isEncrypted){
+        this.password = hashSync(this.password, 10)
+      }
+    }
+
+    @Column({ type: "varchar" })
+    telephone: string
+
+    @CreateDateColumn({ type: "date" })
+    createdAt: string
+
+    @UpdateDateColumn({ type: "date" })
+    updatedAt: string
+
+    @DeleteDateColumn({ type: "date" })
+    deletedAt: string
+}
