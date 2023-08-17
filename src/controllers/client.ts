001881b612c8fdc0d693d1455565141414549323
Carol-Rocha <caroldsr1@gmail.com>
2023-08-17 13:05:05 -0300

    feat: create rotate to update client

--- src/controllers/client.ts
+++ src/controllers/client.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express"
 import { createClientService } from "../services/clients/createClient.service"
-import { TClientRequest } from "../interfaces/clients.interfaces"
+import { TClientRequest, TClientUpdateRequest } from "../interfaces/clients.interfaces"
+import { updateClientService } from "../services/clients/uptadeClient.service"
 
 export const createClientController = async (
   req: Request,
@@ -9,4 +10,15 @@ export const createClientController = async (
   const clientData: TClientRequest = req.body
   const newCLient = await createClientService(clientData)
   return res.status(201).json(newCLient)
+}
+
+export const updateClientController = async (
+  req: Request,
+  res: Response
+): Promise<Response> => {
+  const {clientId} = res.locals
+  const clientData: TClientUpdateRequest = req.body
+  const updatedClient = await updateClientService(clientData, clientId)
+
+  return res.json(updatedClient)
 }
--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
 import { ensureClientExistMiddleware } from "../middlewares/clients/ensureClientExist.middleware";
-import { createClientController } from "../controllers/client";
+import { createClientController, updateClientController } from "../controllers/client";
 import { ensureDataIsValidMiddleware } from "../middlewares/ensureDataIsValid.middleware";
-import { clientSchemaRequest } from "../schemas/clients.schemas";
+import { clientSchemaRequest, updatedClientSchema } from "../schemas/clients.schemas";
 import { verifyEmailExistMiddleware } from "../middlewares/clients/verifyEmailExist.middleware";
 import { verifyUserNameExistMiddleware } from "../middlewares/clients/verifyUserNameExist.middleware";
 
@@ -14,4 +14,13 @@ clientRoutes.post(
   verifyUserNameExistMiddleware,
   verifyEmailExistMiddleware, 
   createClientController
+)
+
+clientRoutes.patch(
+  "/:id",
+  ensureDataIsValidMiddleware(updatedClientSchema),
+  ensureClientExistMiddleware,
+  verifyUserNameExistMiddleware,
+  verifyEmailExistMiddleware, 
+  updateClientController
 )
