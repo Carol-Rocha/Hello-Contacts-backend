49c1b2324011eca754ac5913710ec801fcfa6c7f
Carol-Rocha <caroldsr1@gmail.com>
2023-09-14 19:50:21 -0300

    fix: exclude userName check from routes

--- src/routes/client.routes.ts
+++ src/routes/client.routes.ts
@@ -1,37 +1,41 @@
-import { Router } from "express";
-import { ensureClientExistMiddleware } from "../middlewares/clients/ensureClientExist.middleware";
-import { ensureDataIsValidMiddleware } from "../middlewares/ensureDataIsValid.middleware";
-import { clientSchemaRequest, updatedClientSchema } from "../schemas/clients.schemas";
-import { verifyEmailExistMiddleware } from "../middlewares/clients/verifyEmailExist.middleware";
-import { verifyUserNameExistMiddleware } from "../middlewares/clients/verifyUserNameExist.middleware";
-import { ensureTokenIsValidMiddleware } from "../middlewares/clients/ensureTokenIsValid.middleware";
-import { ensureClientIsOwnerMiddleware } from "../middlewares/clients/ensureClientIsOwner.middleware";
-import { createClientController, deleteClientController, updateClientController } from "../controllers/client.controllers";
-import { verifyClientIsDeletedMiddleware } from "../middlewares/clients/verifyClientIsDeletedMiddleware";
+import { Router } from 'express'
+import { ensureClientExistMiddleware } from '../middlewares/clients/ensureClientExist.middleware'
+import { ensureDataIsValidMiddleware } from '../middlewares/ensureDataIsValid.middleware'
+import {
+  clientSchemaRequest,
+  updatedClientSchema
+} from '../schemas/clients.schemas'
+import { verifyEmailExistMiddleware } from '../middlewares/clients/verifyEmailExist.middleware'
+import { ensureTokenIsValidMiddleware } from '../middlewares/clients/ensureTokenIsValid.middleware'
+import { ensureClientIsOwnerMiddleware } from '../middlewares/clients/ensureClientIsOwner.middleware'
+import {
+  createClientController,
+  deleteClientController,
+  updateClientController
+} from '../controllers/client.controllers'
+import { verifyClientIsDeletedMiddleware } from '../middlewares/clients/verifyClientIsDeleted.middleware'
 
 export const clientRoutes: Router = Router()
 
 clientRoutes.post(
-  "",
+  '',
   ensureDataIsValidMiddleware(clientSchemaRequest),
-  verifyUserNameExistMiddleware,
-  verifyEmailExistMiddleware, 
+  verifyEmailExistMiddleware,
   createClientController
 )
 
 clientRoutes.patch(
-  "/:id",
+  '/:id',
   ensureDataIsValidMiddleware(updatedClientSchema),
   ensureTokenIsValidMiddleware,
   ensureClientExistMiddleware,
   ensureClientIsOwnerMiddleware,
-  verifyUserNameExistMiddleware,
-  verifyEmailExistMiddleware, 
+  verifyEmailExistMiddleware,
   updateClientController
 )
 
 clientRoutes.delete(
-  "/:id",
+  '/:id',
   ensureTokenIsValidMiddleware,
   ensureClientExistMiddleware,
   verifyClientIsDeletedMiddleware,
